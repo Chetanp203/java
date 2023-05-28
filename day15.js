95f198caea18d045b2c595e5320010d07eb56ffd
Chetanp203 <patil.chetan1994@gmail.com>
2023-05-28 10:30:46 +0530

    Create day15.js

---
+++ day15.js
@@ -0,0 +1,49 @@
+// datatypes
+// String, number, Boolean
+// variables
+// console.log();
+// typeof();
+
+
+// var number1 = 10
+// var number2 = 20
+
+// var addition = number1 + number2;
+// console.log(addition)
+// var substraction = number1 - number2;
+// console.log(substraction)
+// var multiply = number1*number2;
+// console.log(multiply)
+// var division = number1/number2;
+// console.log(division)
+
+
+
+// var i=1;
+// console.log(i)
+// i=i+1;
+// i++;
+// i++;
+// i++;
+// i++;
+// console.log(i)
+
+
+// var number = 100;
+// console.log(number>150)
+// console.log(number<150)
+
+
+// Q. Check the given number is lesser than 100, 150 & 200
+
+// var number = 270;
+
+// if (number < 100) {
+//     console.log("Number is less than 100.")
+// } else if (number < 150) {
+//     console.log("Number is less than 150 grater than 100")
+// } else if ( number < 200) {
+//     console.log("Number is less than 200 greater than 150")
+// } else {
+//     console.log("Number is not less than 100, 150 and 200")
+// }
