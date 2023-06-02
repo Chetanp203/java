f3830b09a1a3b72f814ff835f383b62d3e67323c
Chetanp203 <patil.chetan1994@gmail.com>
2023-06-02 16:13:48 +0530

    update

--- day18.js
+++ day18.js
@@ -17,14 +17,29 @@
 // }
 
 
-var target=20;
-var array=[2, 34, 23, 50, 30, 45, 16];
+// var target=20;
+// var array=[2, 34, 23, 50, 30, 45, 16];
 
- for(var i=0; i<array.length;i++){
+//  for(var i=0; i<array.length;i++){
     
-    for(var j=i+1; j<array.length;j++){
-        if(array[i]-array[j]==target){
-            console.log(array[1],array[j])
-        }
-    }
- }
+//     for(var j=i+1; j<array.length;j++){
+//         if(array[i]-array[j]==target){
+//             console.log(array[i],array[j])
+//         }
+//     }
+//  }
+
+
+// var target=70;
+// var array=[2, 35, 30, 20, 25, 15, 56];
+
+// for(var i=0; i<array.length-2; i++){
+//     for(var j=i+1; j<array.length-1; j++){
+//         for(var k=j+1; k<array.length;k++){
+//             if(array[i]+array[j]+array[k]==target){
+//                 console.log(array[i],array[j],array[k])
+//             }
+//         }
+//     }
+// }
+
