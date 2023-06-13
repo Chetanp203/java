779be4bba7ad86f07fb4bd475044e5f62d153882
Chetanp203 <patil.chetan1994@gmail.com>
2023-06-13 10:39:18 +0530

    update

---
+++ day26.js
@@ -0,0 +1,35 @@
+
+// function resolveAfter2Seconds() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve('response');
+//         }, 5000);
+//     });
+// }
+
+// async function asyncCall() {
+//     console.log('Start');
+//     const result = await resolveAfter2Seconds();
+//     console.log(result,"- result");
+//     console.log("end")
+//     // Expected output: "resolved"
+// }
+// asyncCall();
+
+
+// function backin4seconds(){
+//     return new Promise((resolve) =>{
+//         setTimeout(()=>{
+//             resolve('i am back');
+//         }, 4000);
+//     })
+// }
+
+// async function callback() {
+//     console.log('Hello');
+//     const result = await backin4seconds();
+//     console.log(result, "-How are you");
+//     console.log('bye')
+// }
+// callback();
+
