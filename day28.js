077bb83d8f6002a9782c44584c7d9b5f7fdb1a9b
Chetanp203 <patil.chetan1994@gmail.com>
2023-06-18 10:16:01 +0530

    update

---
+++ day28.js
@@ -0,0 +1,72 @@
+// function nameoffunction (hello){
+//       console.log(hello)
+// }
+// nameoffunction("hey")
+
+
+// var nameoffunction2 = (helloo) => {
+//     console.log(helloo)
+// }
+// nameoffunction2("hiii")
+
+
+// const NameOfFunction3 = props => {
+//     console.log(props)
+// }
+// NameOfFunction3("hiii")
+
+
+// const NameOfFunction4 = (props, props2) => console.log(props,props2);
+
+// NameOfFunction4("ji", "hello");
+
+// var myname ="chetan";    -----declare----
+// myname="karan"  ----re-assign----
+// var myname ="patil";
+// console.log(myname)
+// re-declare and re-assign are possible in var
+
+
+// let myname ="chetan";
+// myname ="patil";
+// let myname ="karan"
+// console.log(myname)
+// you can re-assign but not re-declare in let
+
+
+// const myname ="chetan";
+// myname="patil";
+// console.log(myname)
+// you can niether re-assign nor re-declare in const
+
+
+// myname="patil";
+// console.log(myname)
+
+// // hoisting.....can assign before still it will get the value declared afterwards
+
+// var myname
+
+
+
+
+// Closure 
+
+
+
+// function outerFunction() {
+//     var myMarks = 40;
+//     console.log("Inside outer function");
+//     function innerFunction() {
+//         console.log("Inside inner Function", myMarks)
+//     }
+//     return innerFunction;
+// }
+// const res = outerFunction();
+// // res();
+// console.log(res())
+
+// // secret - res ke under function store hoga with varible call myMarks
+
+// // console.log(res())
+
