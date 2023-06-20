fe7806ad8ec2a1dee922ae4a6073722cb8b20bcb
Chetanp203 <patil.chetan1994@gmail.com>
2023-06-20 18:25:36 +0530

    update

--- day29.js
+++ day29.js
@@ -1,9 +1,18 @@
-nums =[1,1,2,2,3,3]
+// nums =[1,1,2,2,3,3]
 
-for( var i=0 ; i < nums.length-1;i++){
-    for(j=i+1 ; j< nums.length; j++){
-        if(nums[i]=nums[j+1]){
-            console.log([i,j])
-        }
-    }
-}
+// for( var i=0 ; i < nums.length-1;i++){
+//     for(j=i+1 ; j< nums.length; j++){
+//         if(nums[i]=nums[j+1]){
+//             console.log([i,j])
+//         }
+//     }
+// }
+
+
+var nums = [1,3,5,6];
+ var target = 5;
+
+ for (var i = 0; i < nums.length; i++) {
+    var index = nums.findIndex((object) => nums[i] == target);
+    console.log(index)
+ }
