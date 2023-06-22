5d8753ddd29b16b967d7ffde73513e6281526a47
Chetanp203 <patil.chetan1994@gmail.com>
2023-06-22 14:03:59 +0530

    update

--- day29.js
+++ day29.js
@@ -9,10 +9,38 @@
 // }
 
 
-var nums = [1,3,5,6];
- var target = 5;
+// var nums = [1,3,5,6];
+//  var target = 5;
 
- for (var i = 0; i < nums.length; i++) {
-    var index = nums.findIndex((object) => nums[i] == target);
-    console.log(index)
- }
+//  for (var i = 0; i < nums.length; i++) {
+//     var index = nums.findIndex((object) => nums[i] == target);
+//     console.log(index)
+//  }
+
+
+//  nums = [2,2,1];
+//  var answer =[];
+//  var removeElement = function(nums){
+// for (var i=0; i<nums.length; i++){
+    
+//     if( nums[i] == nums[i+1]){
+//     //   answer.push();
+//     if( nums[i] != nums[i+1]){
+//         answer.push(nums[i]);
+//     }
+//     }
+// }
+// return answer;
+//  }
+nums = [0,0,2,4,1,2]
+var singleNumber = function(nums) {
+    let n = nums.length;
+    let c = 0;
+    for (let i = 0; i < n; i++) {
+    //    console.log(nums[i])
+        c = c ^ nums[i];
+    }
+    return c; 
+}
+var result = singleNumber(nums);
+console.log(result)
