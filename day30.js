64c9a664772c62c8479914a5f27aa6fff7c7a3be
Chetanp203 <patil.chetan1994@gmail.com>
2023-06-28 23:10:09 +0530

    update

--- day30.js
+++ day30.js
@@ -21,17 +21,44 @@
 // console.log(res)
 
 
-letters = ["c","f","j"];
-target ="a";
+// letters = ["c","f","j"];
+// target ="a";
 
 
-var greaterNumbers = function(letters) {
-     for (let i = 0; i < letters.length; i++) {
-         if (letters[i] > target) 
-         return letters[i];
-     }
+// var greaterNumbers = function(letters) {
+//      for (let i = 0; i < letters.length; i++) {
+//          if (letters[i] > target) 
+//          return letters[i];
+//      }
      
- }
-    let res=greaterNumbers(letters);
-     console.log(res)
+//  }
+//     let res=greaterNumbers(letters);
+//      console.log(res)
 
+
+
+// Q no 747
+
+nums = [3,6,1,0]
+index=0;
+count=0;
+let max = nums[0];
+var largestNumber =function(nums){
+    for(i=1; i< nums.length;i++){
+        if(nums[i]>max){
+            max=nums[i];
+            index=i;
+        }
+    }
+    for(i=0; i<nums.length;i++){
+        if(i!=index && max>=2*nums[i]){
+            count++;
+        }
+    }
+    if(count==nums.length-1)
+    return index;
+    else
+    return -1;
+}
+let res = largestNumber(nums)
+console.log(res);
