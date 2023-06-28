6652c4a09332761ad18666141d0a9e9302e7e805
Chetanp203 <patil.chetan1994@gmail.com>
2023-06-28 15:19:21 +0530

    update

--- day30.js
+++ day30.js
@@ -1,21 +1,37 @@
 
-nums = [1,1,0,1,1,1]
+// nums = [1,1,0,1,1,1]
 
-var findmaxconsecutiveones = function(nums){
-    let maxconsec =0;
-    let currentconsec =0;
+// var findmaxconsecutiveones = function(nums){
+//     let maxconsec =0;
+//     let currentconsec =0;
 
-    for(let i=0; i< nums.length; i++){
-        if(nums[i] === 1){
-            currentconsec++;
-        }else{
-            currentconsec =0;
-        }
-        if (currentconsec > maxconsec){
-            maxconsec = currentconsec;
-        }
-    }
-    return maxconsec;
-}
-let res =findmaxconsecutiveones(nums)
-console.log(res)
+//     for(let i=0; i< nums.length; i++){
+//         if(nums[i] === 1){
+//             currentconsec++;
+//         }else{
+//             currentconsec =0;
+//         }
+//         if (currentconsec > maxconsec){
+//             maxconsec = currentconsec;
+//         }
+//     }
+//     return maxconsec;
+// }
+// let res =findmaxconsecutiveones(nums)
+// console.log(res)
+
+
+letters = ["c","f","j"];
+target ="a";
+
+
+var greaterNumbers = function(letters) {
+     for (let i = 0; i < letters.length; i++) {
+         if (letters[i] > target) 
+         return letters[i];
+     }
+     
+ }
+    let res=greaterNumbers(letters);
+     console.log(res)
+
